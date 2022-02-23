d10bde32bb28ec5e6923840c2d1f8b3e4eabe447
Peter <warrenp11@gmail.com>
2022-02-23 12:38:19 -0500

    Recode Initial Game Logic

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,16 @@
-// Capture data (user input)
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight() {
-  window.alert("The fight has begun!");
-}
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-// fight();
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+};
+
+fight();
