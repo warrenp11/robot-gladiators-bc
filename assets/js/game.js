deedbff1ad13ca693e8a213e3d015da3f7eddfd4
Peter <warrenp11@gmail.com>
2022-02-23 12:34:16 -0500

    Setting Up playerName Variable

---
+++ assets/js/game.js
@@ -0,0 +1,9 @@
+// Capture data (user input)
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+// fight();
